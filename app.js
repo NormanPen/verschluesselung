4e97971018d4fb7e582f8e0193cb24d11c090522
Norman Pendzich <norman.pendzich@outlook.de>
2022-08-02 09:46:25 +0200

    Kommentare hinzugefügt

--- app.js
+++ app.js
@@ -1,53 +1,95 @@
 
+
+// Den folgenden Text wollen wir verschluesseln.
+// Der Ausgangstext wird "Klartext" oder auf englisch "plain text" genannt.
 var plainText = prompt("Text eingeben");
+
+// Um diese Anzahl wollen wir die Zeichen im Alphabet verschieben.
+// Ein positiver Wert verschiebt nach rechts, ein negativer nach links.
 var verschiebung = 1;
+
+// In dieser Variable speichern wir den verschluesselten Text.
+// Der verschluesselte Text wird auf englisch "cipher text" genannt.
 var cipherText;
 
-
+// Hier Hilfsvariablen mit den ASCII Codes von a, z, A und Z.
+// Die werden wir spaeter gleich brauchen
 var charCodeKleinA = "a".charCodeAt(0);
 var charCodeKleinZ = "z".charCodeAt(0);
 var charCodeGrossA = "A".charCodeAt(0);
 var charCodeGrossZ = "Z".charCodeAt(0);
 
-
+// Hier machen wir die eigentliche Transformation eines Zeichens.
 function zeichenTransformieren(charCode, startAsciiCode) {
-
-  var indexImAlphabet = charCode - startAsciiCode;
-  var transformiertCode = indexImAlphabet + verschiebung;
-
-  if (transformiertCode > 25) {
-    transformiertCode -= 26;
-  }
-  else if (transformiertCode < 0) {
-    transformiertCode += 26;
-  }
-  return startAsciiCode + transformiertCode;
+	// charCode: Enthält den ASCII Code des Ausgangszeichens, das
+	//           wir transformieren wollen
+	// startAsciiCode: ASCII Code des ersten Zeichens des 
+	//                 Alphabets (97 fuer "a" oder 65 fuer "A")
+	
+	// Als erstes berechnen wir, an welcher Stelle im Alphabet das
+	// Ausgangszeichen steht. Achte darauf, dass dieser Wert mit
+	// 0 (Null) zu zählen beginnt, nicht mit 1. 0 steht also für 
+	// a bzw. A, 1 für b bzw. B, ...
+	var indexImAlphabet = charCode - startAsciiCode;
+	
+	// Jetzt verschieben wir.
+	var transformiertCode = indexImAlphabet + verschiebung;
+	
+	// Wenn wir ueber Z hinaus gehen, fangen wir am Anfang wieder an.
+	// Das machen wir, indem wir die Anzahl an Buchstaben des deutschen
+	// Alphabets (26) abziehen. 
+	if (transformiertCode > 25) { transformiertCode -= 26; }
+	
+	// Beim Verschieben nach Links können wir vor dem A landen. In diesem
+	// Fall muessen wir zur Korrektur die Anzahl an Buchstaben im deutschen
+	// Alphabets (26) addieren.
+	else if (transformiertCode < 0) { transformiertCode += 26; }
+	
+	// Jetzt können wir den Index im Alphabet wieder in ASCII umwandeln,
+	// indem wir 97 fuer "a" oder 65 fuer "A" addieren.
+	return startAsciiCode + transformiertCode;
 }
 
+// Hier machen wir die eigentliche Transformation des Textes
 function textTransformieren(ausgangstext) {
-  var ergebnis = "";
+	// In dieser Variable bauen wir unser Ergebnis. Wir starten
+	// mit einem leeren Text.
+	var ergebnis = "";
 
-  for (var i = 0; i < ausgangstext.length; i++) {
-    var charCode = ausgangstext.charCodeAt(i);
+	// Wir starten mit einer Schleife ueber alle Zeichen des Textes. 
+	for (var i = 0; i < ausgangstext.length; i++) {
+		// Wir holen uns den ASCII Code des aktuellen Zeichens
+		var charCode = ausgangstext.charCodeAt(i);
+	
+		// Ist es ein Buchstabe zwischen A und Z?
+		if (charCode >= charCodeGrossA && charCode <= charCodeGrossZ) {
+			// Verschluesseln ausgehend von "A"
+			charCode = zeichenTransformieren(charCode, charCodeGrossA);
+		}
+		// Ist es ein Buchstabe zwischen a und z?
+		else if (charCode >= charCodeKleinA && charCode <= charCodeKleinZ) {
+			// Verschluesseln ausgehend von "a"
+			charCode = zeichenTransformieren(charCode, charCodeKleinA);
+		}
 
-    if (charCode >= charCodeGrossA && charCode <= charCodeGrossZ) {
-
-      charCode = zeichenTransformieren(charCode, charCodeGrossA);
-    }
-    else if (charCode >= charCodeKleinA && charCode <= charCodeKleinZ) {
-      charCode = zeichenTransformieren(charCode, charCodeKleinA);
-    }
-    ergebnis += String.fromCharCode(charCode);
-  }
-
-  return ergebnis;
+		// Wenn es kein Buchstabe sondern z.B. ein Sonderzeichen ist, 
+		// lassen wir das Zeichen unverändert.
+	
+		ergebnis += String.fromCharCode(charCode);
+	}
+	
+	return ergebnis;
 }
 
+// Verschlüsseln
 cipherText = textTransformieren(plainText);
 
+// Zur Kontrolle geben wir den verschluesselten Text aus.
 console.log("Aus '" + plainText + "' wird '" + cipherText + "'.");
 
+// Entschlüsseln
 verschiebung *= -1;
 plainText = textTransformieren(cipherText);
 
-alert("Aus '" + cipherText + "' wird '" + plainText + "'.");
+// Zur Kontrolle geben wir den entschluesselten Text aus.
+prompt("Aus '" + cipherText + "' wird '" + plainText + "'.");
